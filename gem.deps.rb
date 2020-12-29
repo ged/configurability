e17783576711f6c5bca581d2000f556faf2e7b55
Michael Granger <ged@FaerieMUD.org>
2020-12-28 16:24:23 -0800

    Update signing cert, bump rake-deveiate dependency for Ruby 3.

--- gem.deps.rb
+++ gem.deps.rb
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'loggability', '~> 0.15'
 
 group :development do
-	gem 'rake-deveiate', '~> 0.10'
+	gem 'rake-deveiate', '~> 0.17'
 	gem 'simplecov', '~> 0.12'
 	gem 'rspec', '~> 3.9'
 end
